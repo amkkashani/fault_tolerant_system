d93b75dfe33ee61b158f4f044299f580b44f8391
KASHANI <amkkashani@gmail.com>
2022-08-07 20:00:12 +0430

    graph resource management completed

--- genetic.py
+++ genetic.py
@@ -1,27 +1,41 @@
 import random
+import time
 from typing import List
+
+import main
 from main import Task
-from main import graph
+from main import Graph
+from main import Node
+from main import Cluster
+from main import *
 
 Genome = List[Task]
 Population = List[Genome]
 
 
 def generate_genome(tasks: List[Task]):
+    graph = main.setup_problem_clusters()
+    random.seed(time.time())
     for task in tasks:
         task.received_throughput = [0] * len(task.vertices)
         start_nodes = set()
 
         for i, vertex in enumerate(task.vertices):
-            if vertex.start in start_nodes:
+            if vertex.start in start_nodes:  # initial heuristic
                 continue
+            if vertex.end == -2:
+                continue  # it is end node and no need to get resource
 
             start_nodes.add(vertex.start)
-            task.received_throughput[i] = random.randint(1, task.replica + 1)
+            random_replica = random.randint(1, task.replica + 1)
+            accepted_replica = task.demand_throughput(vertex.end, task.usage, random_replica, graph)
+            task.received_throughput[i] = accepted_replica
+
 
 
 def generate_population(size: int, tasks: List[Task]):
     return [generate_genome(tasks) for _ in range(size)]
 
-def fitness_function(genome : Genome) -> int:
+
+def fitness_function(genome: Genome) -> int:
     yield
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
+import random
+
 import genetic
 from typing import List
 
-graph = None
 
 
 # vertex in cluster graph
@@ -24,7 +25,7 @@ class Cluster:
         self.nodes = nodes
         self.states = []
         for node in nodes:
-            self.state = node.capacity
+            self.states.append(node.capacity)
 
     def request(self, usage, replica=1):
         temp_state = self.states.copy()
@@ -43,32 +44,32 @@ class Cluster:
     def accept(self, temp_state):
         self.state = temp_state
 
+    def request_max_available(self, usage, replica=1):
+        temp_state = self.states.copy()
+        reserve_node = 0
+        for i in range(replica):
+            id = self.find_min(temp_state, usage)
+            if id == -1:
+                # not found
+                break
+            else:
+                temp_state[id] -= usage
+                reserve_node += 1
+
+        self.accept(temp_state)
+        return reserve_node
+
+
     def find_min(self, ls: list[int], cap):
         min = math.inf
         min_id = -1
         for i in range(len(ls)):
-            if self.ls[i] < cap:  # node i is not good enough
+            if ls[i] < cap:  # node i is not good enough
                 continue
-            if self.ls[i] < min:
-                min = self.ls[i]
+            if ls[i] < min:
+                min = ls[i]
                 min_id = i
         return min_id
-
-
-class Task:
-    def __init__(self, vertices: list[Vertex], replica):
-        self.vertices = vertices
-        self.replica = replica
-        self.received_throughput = [0] * len(vertices)
-
-    def set_received_througput(self, index, amount):
-        self.received_throughput[index] = amount
-
-    def get_resource(self):
-        yield
-
-    def finished(self):
-        yield
 
 
 # graph of clusters
@@ -82,11 +83,56 @@ class Graph:
     def add_cluster(self, cluster: Cluster):
         self.clusters[cluster.name] = cluster
 
+    def reserved_capacity(self, cluster_name, usage , replica):
+        number_of_reserved_node = self.clusters[cluster_name].request_max_available(usage , replica)
+        return number_of_reserved_node
+
+class Task:
+    def __init__(self, vertices: list[Vertex], replica , usage):
+        self.vertices = vertices
+        self.usage = usage
+        self.replica = replica
+        self.received_throughput = [0] * len(vertices)
+
+    # def set_received_throughput(self, index, amount):
+    #     self.received_throughput[index] = amount
+    #
+
+    def demand_throughput(self, cluster_name, usage, replica, target_graph: Graph) -> int:
+        gained_nodes =  target_graph.reserved_capacity(cluster_name, usage, replica)
+        return gained_nodes
+
+    def get_resource(self):
+        yield
+
+    def finished(self):
+        yield
+
 
 def main():
-    print("hello")
-    graph = Graph()
+    # print("hello")
+    # graph = Graph()
+    setup_problem_clusters()
 
+    setup_problem_clusters()
+
+
+
+def setup_problem_clusters():
+    number_of_clusters = 15
+    clusters = []
+    random.seed(1500)
+    nodes = []
+    for i in range(1000):
+        nodes.append(Node(random.randint(0, 10)))
+
+    # iteration starts from 1 because source cluster is 0 and the other starts from 1
+    for i in range(1, number_of_clusters + 1):
+        cluster_nodes = random.randint(5, 15)
+        clusters.append(Cluster(i, nodes[0: cluster_nodes]))
+        nodes = nodes[cluster_nodes:]
+
+    return Graph(clusters)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
